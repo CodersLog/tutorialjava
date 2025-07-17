c077a7dbf22857cbfb62cd634e934b3c32163dd0
CodersLog <rhcjanssen@hotmail.com>
2025-07-17 21:12:41 +0200

    variables

---
+++ src/Variables.java
@@ -0,0 +1,54 @@
+public abstract class Variables {
+    public static void main(String[] args) {
+
+    // this is a comment
+
+        /*
+        this is a multi line comment
+        it is used to explain stuff
+         */
+
+    //variable
+    //something that has a name and a value
+
+    // ; is a declaration always input after a variable
+
+    // int is a value between -2,147,483,648 to 2,147,483,647
+    // ints are datatypes that must be named and must be whole numbers
+    int Int = 456;
+    // it can be declared after creation for example:
+    int exampleint;
+    exampleint = 100;
+
+    // floats and doubles can be used to give numbers with decimal points
+    // however they are not always accurate
+    float enumfloat = 20.20304002f;
+    double enumdouble = 462.2847865;
+    //they can also be declared after creation
+    float examplefloat;
+    examplefloat = 2.4526784589f;
+
+    double exampledouble;
+    exampledouble = 2.38677469234;
+
+    //booleans can be true or false
+    boolean hasstamina = false;
+    boolean wantsstamina = true;
+
+    // can also be declared after creation
+
+    boolean isUpFacing;
+    isUpFacing=false;
+
+    // Chars and strings are letter based variables that must be declared in '' or ""
+    // Chars must be incapsulated between '' and must be single characters
+    // Strings must be incapsulated between "" and can be whole texts
+    //they can also be declared after creation
+
+    char grade;
+    grade = 'B';
+
+    String name;
+    name = "help me";
+
+}}
